3cfbc50a12481e17a7e40e77f3b0d1e3e36a3017
BennoAlif <bennoalif41@gmail.com>
2019-09-26 12:28:27 +0700

    initial commit

---
+++ src/com/benno/Main.java
@@ -0,0 +1,20 @@
+package com.benno;
+
+    /*
+    NAMA        : BENNO ALIF ANGGARA
+    KELAS       : IF1
+    NIM         : 10118024
+    Deskripsi   : Implementasi operasi logika
+     */
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+}
